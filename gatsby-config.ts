d2d03843d9ed46fdb157aa9b03be48182773d657
makkut <mak-kutprot@mail.ru>
2022-12-29 14:18:31 +0100

    footer, header, info, hamburger

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,23 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    }
+
+  ]
 };
 
 export default config;
